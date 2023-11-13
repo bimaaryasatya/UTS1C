3f0a4cfffe50001bea0750855ea047588927a2d5
bimasunuaryasatya <bimarailfansslw69@gmail.com>
2023-11-13 09:56:32 +0700

    Soal Nomor 3

---
+++ src/ConditionalStatement.java
@@ -0,0 +1,9 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int Plat_Nomor_Digit_1 = 8;
+        int Plat_Nomor_Digit_2 = 5;
+        int Plat_Nomor_Digit_3 = 7;
+        int Plat_Nomor_Digit_4 = 2;
+        if()
+    }
+}
